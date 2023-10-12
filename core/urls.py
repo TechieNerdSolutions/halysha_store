b77110e06406c5702b9f4f051d765767023cf9f6
Rachamv <rkutalian@gmail.com>
2023-10-12 03:15:15 +0100

    halysha version 2.0

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,16 @@
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views
 from django.urls import path
 
-from . import views
-from .forms import LoginForm
-
-app_name = 'core'
+from core.views import frontpage, shop, signup, myaccount, edit_myaccount
+from product.views import product
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contact/', views.contact, name='contact'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
+    path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('myaccount/', myaccount, name='myaccount'),
+    path('myaccount/edit/', edit_myaccount, name='edit_myaccount'),
+    path('shop/', shop, name='shop'),
+    path('shop/<slug:slug>/', product, name='product'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,32 +1,68 @@
+import re
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, redirect
 
-from item.models import Category, Item
+from product.models import Product, Category
 
-from .forms import SignupForm
+from .forms import SignUpForm
 
-def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6]
-    categories = Category.objects.all()
+def frontpage(request):
+    products = Product.objects.all()[0:8]
 
-    return render(request, 'core/index.html', {
-        'categories': categories,
-        'items': items,
-    })
-
-def contact(request):
-    return render(request, 'core/contact.html')
+    return render(request, 'core/frontpage.html', {'products': products})
 
 def signup(request):
     if request.method == 'POST':
-        form = SignupForm(request.POST)
+        form = SignUpForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            user = form.save()
 
-            return redirect('/login/')
+            login(request, user)
+
+            return redirect('/')
     else:
-        form = SignupForm()
+        form = SignUpForm()
 
-    return render(request, 'core/signup.html', {
-        'form': form
-    })
+    return render(request, 'core/signup.html', {'form': form})
+
+@login_required
+def myaccount(request):
+    return render(request, 'core/myaccount.html')
+
+@login_required
+def edit_myaccount(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
+        user.email = request.POST.get('email')
+        user.username = request.POST.get('username')
+        user.save()
+
+        return redirect('myaccount')
+    return render(request, 'core/edit_myaccount.html')
+
+def shop(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    active_category = request.GET.get('category', '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    context = {
+        'categories': categories,
+        'products': products,
+        'active_category': active_category
+    }
+
+    return render(request, 'core/shop.html', context)
